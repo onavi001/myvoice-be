1d7a52110d5b1a4bf723d23e70206b1a1b75d5ca
onavi001 <34868568+onavi001@users.noreply.github.com>
2025-04-10 00:36:49 -0600

    update MD

--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,7 @@ export function middleware(request: Request) {
   const response = NextResponse.next();
 
   // Permitir solicitudes desde el origen del FE
-  response.headers.set("Access-Control-Allow-Origin", "http://localhost:5173"); // Cambia "*" por tu dominio FE en producción
+  response.headers.set("Access-Control-Allow-Origin", "*"); // Cambia "*" por tu dominio FE en producción
   response.headers.set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   response.headers.set("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
