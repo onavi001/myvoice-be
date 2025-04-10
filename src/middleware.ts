9467f6105c30b8c0e895d26660f4f0207d2b25c7
onavi001 <34868568+onavi001@users.noreply.github.com>
2025-04-10 00:46:21 -0600

    up MD

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,7 +4,7 @@ export function middleware(request: Request) {
   const response = NextResponse.next();
 
   // Permitir el origen del FE en desarrollo
-  response.headers.set("Access-Control-Allow-Origin", "http://localhost:5173"); // Cambia a tu dominio en producción
+  response.headers.set("Access-Control-Allow-Origin", "*"); // Cambia a tu dominio en producción
   response.headers.set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   response.headers.set("Access-Control-Allow-Headers", "Content-Type, Authorization");
   response.headers.set("Access-Control-Allow-Credentials", "true"); // Si usas cookies
